6e23cb86758a361e2f2b7d37999f31b1b59112aa
Your Name <you@example.com>
2025-01-29 21:38:53 +0530

    first commit

---
+++ src/gemini.js
@@ -0,0 +1,34 @@
+let apiKey="AIzaSyB1AmAjwk_2pWShZcNR15xTGT2nQinz3JM"
+
+import {
+    GoogleGenerativeAI,
+    HarmCategory,
+    HarmBlockThreshold,
+  } from "@google/generative-ai";
+  
+  const genAI = new GoogleGenerativeAI(apiKey);
+  
+  const model = genAI.getGenerativeModel({
+    model: "gemini-1.5-flash",
+  });
+  
+  const generationConfig = {
+    temperature: 1,
+    topP: 0.95,
+    topK: 40,
+    maxOutputTokens: 8192,
+    responseMimeType: "text/plain",
+  };
+  
+  async function run(prompt) {
+    const chatSession = model.startChat({
+      generationConfig,
+      history: [
+      ],
+    });
+  
+    const result = await chatSession.sendMessage(prompt);
+    return result.response.text()
+  }
+  
+ export default run;
